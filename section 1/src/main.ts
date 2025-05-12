90f909251dabedbcc9b53838180e16e8a145c6b8
phuoctmse <phuoctmse184067@fpt.edu.vn>
2025-05-12 23:04:30 +0700

    interceptor with response

--- section 1/src/main.ts
+++ section 1/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './interceptor/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,7 @@ async function bootstrap() {
     whitelist: true, // tự động loại bỏ các field ko đc khai báo decorator trong DTO
     forbidNonWhitelisted: true, //field ko có trong DTO sẽ báo lỗi
   }));
+  app.useGlobalInterceptors(new TransformInterceptor())
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
